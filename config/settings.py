4dea63f0f39fa00a051539da0d2c98edf853298b
Nath_Kash <nath_kash@MacNath-Kash.local>
2026-05-29 09:22:21 +0100

    Fix CSRF + statiques

--- config/settings.py
+++ config/settings.py
@@ -100,3 +100,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # ── DIVERS ──
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login/'
+
+
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='http://localhost:8000', cast=Csv())
