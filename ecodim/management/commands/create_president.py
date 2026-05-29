4c5ea7b151d87c6905c15c5829331ad86d1660f4
Nath_Kash <nath_kash@MacNath-Kash.local>
2026-05-29 09:11:46 +0100

    Fix statiques + droits président

--- ecodim/management/commands/create_president.py
+++ ecodim/management/commands/create_president.py
@@ -1,19 +1,27 @@
 from django.core.management.base import BaseCommand
-from ecodim.models import User
+from core.models import User
 
 class Command(BaseCommand):
     help = 'Crée le compte président par défaut'
 
     def handle(self, *args, **kwargs):
         if not User.objects.filter(username='president').exists():
-            User.objects.create_superuser(
-                username='president',
-                password='ecodim2024',
+            user = User.objects.create_superuser(
+                username='nath_kash',
+                password='admin123',
                 first_name='Président',
                 last_name='Écodim',
                 role='president'
             )
+            user.is_staff = True
+            user.is_superuser = True
+            user.save()
             self.stdout.write(self.style.SUCCESS('✅ Compte président créé !'))
-            self.stdout.write(self.style.WARNING('⚠️  Changez le mot de passe après connexion !'))
         else:
-            self.stdout.write('ℹ️  Compte président déjà existant')
+            # Met à jour si déjà existant
+            user = User.objects.get(username='president')
+            user.is_staff = True
+            user.is_superuser = True
+            user.set_password('ecodim2024')
+            user.save()
+            self.stdout.write(self.style.SUCCESS('✅ Compte président mis à jour !'))
